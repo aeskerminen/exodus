7831eadd3423cf70d4e9760c9250293df7bd2f1d
aeskerm <kerminenartturi@gmail.com>
2024-09-16 20:39:19 +0300

    Make backend changes from removing rentalAssignments

--- exodus-api/src/routes/router.js
+++ exodus-api/src/routes/router.js
@@ -21,7 +21,7 @@ router.get("/rentalAssignments", async (req, res) => {
   });
 });
 
-router.post("/rentalAssignment", async (req, res) => {
+router.post("/rentalAssignments", async (req, res) => {
   const body = req.body;
   console.log(body);
 
@@ -37,6 +37,26 @@ router.post("/rentalAssignment", async (req, res) => {
   ];
 
   pool.query(query_text, query_values, (err, result) => {
+    if (err) {
+      console.error("Error executing query", err);
+    } else {
+      console.log("Query result:", result.rows);
+    }
+
+    client.release();
+
+    return res.status(200).json(result.rows[0]).end();
+  });
+});
+
+router.delete("/rentalAssignments/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const client = await pool.connect();
+
+  const query_text = `DELETE FROM rental_assignments WHERE id = ${id}`;
+
+  pool.query(query_text, (err, result) => {
     if (err) {
       console.error("Error executing query", err);
     } else {
