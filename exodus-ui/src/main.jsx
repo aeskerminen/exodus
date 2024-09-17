41b2f9b574bab24ed2ed6ca5713b40978467f53e
aeskerm <kerminenartturi@gmail.com>
2024-09-17 12:56:50 +0300

    add routing

--- exodus-ui/src/main.jsx
+++ exodus-ui/src/main.jsx
@@ -3,9 +3,29 @@ import App from "./App.jsx";
 import "./index.css";
 import { store } from "./store.js";
 import { Provider } from "react-redux";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import BoxRentalManager from "./components/BoxRentalManager.jsx";
+import WarehouseRentalManager from "./components/WarehouseRentalManager.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Outlet></Outlet>,
+    children: [
+      {
+        path: "boxes",
+        element: <BoxRentalManager></BoxRentalManager>,
+      },
+      {
+        path: "warehouse",
+        element: <WarehouseRentalManager></WarehouseRentalManager>,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </Provider>
 );
