e5185e87d2ec9cf710b03ddc0264f0349406c555
Artturi Kerminen <kerminenartturi@gmail.com>
2024-09-24 16:22:23 +0300

    update app.js for last commit

--- exodus-api/src/app.js
+++ exodus-api/src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const rentalAssignmentRouter = require("./routes/rentalAssignmentRouter");
+const warehouseRentalRouter = require("./routes/warehouseRentalRouter");
 const app = express();
 
 app.use(express.json());
@@ -9,5 +10,6 @@ app.use(morgan("tiny"));
 app.use(cors());
 
 app.use("/data/rentalAssignments", rentalAssignmentRouter);
+app.use("/data/warehouseRentals", warehouseRentalRouter);
 
 module.exports = app;
