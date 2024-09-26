222cd56f73a18911fd0f011bec9734584268cfcc
aeskerm <kerminenartturi@gmail.com>
2024-09-26 09:31:10 +0300

    Change management screen ui

--- exodus-ui/src/components/ManagementScreen.jsx
+++ exodus-ui/src/components/ManagementScreen.jsx
@@ -2,18 +2,18 @@ import { Link } from "react-router-dom";
 
 const ManagementScreen = () => {
   return (
-    <div className="w-full h-full p-4">
+    <div className="w-screen h-screen p-4 flex justify-center items-center">
       <div className="flex justify-center items-center gap-2">
         <Link
           style={{ backgroundColor: "#343434" }}
-          className="p-2 shadow rounded-lg w-fit border-2"
+          className="p-3 shadow rounded-lg w-fit shadow"
           to={"/management/boxes"}
         >
           Box rental manager
         </Link>
         <Link
           style={{ backgroundColor: "#343434" }}
-          className="p-2 shadow rounded-lg w-fit border-2"
+          className="p-3 shadow rounded-lg w-fit shadow"
           to={"/management/warehouse"}
         >
           Warehouse rental manager
